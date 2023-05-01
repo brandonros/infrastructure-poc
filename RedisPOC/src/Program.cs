602ebcb3195b9613552cd30c02e7559c3eae24df
Brandon Ros <brandonros1@gmail.com>
2023-05-01 12:25:56 -0400

    fix password

--- RedisPOC/src/Program.cs
+++ RedisPOC/src/Program.cs
@@ -3,7 +3,7 @@
 namespace RedisPOC
 {
     class Program {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             // connect
             string connectionString = System.Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING");
@@ -13,6 +13,8 @@ namespace RedisPOC
             redisDatabase.StringSet("foo", "bar");
             // get
             System.Console.WriteLine(redisDatabase.StringGet("foo"));
+            // return
+            return 0;
         }
     }
 }
