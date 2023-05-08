e9c1c79ac828564bfc16c9d5f799d6bd98359a46
Brandon Ros <brandonros1@gmail.com>
2023-05-08 14:15:50 -0400

    sync on cicd script

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -10,3 +10,7 @@ cd ..
 # apply
 kubectl apply -f apps/
 # TODO: trigger argocd app sync?
+ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o json | jq -r '.data.password' | base64 --decode --ignore-garbage)
+argocd login localhost:8080 --username admin --password "$ARGOCD_PASSWORD" --insecure
+argocd app sync microservice-poc1
+argocd app sync microservice-poc2
