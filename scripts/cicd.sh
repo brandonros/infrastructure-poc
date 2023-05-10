3e3286c2dbedd42a71aa32cbbdf4f9aea4835ec2
Brandon Ros <brandonros1@gmail.com>
2023-05-10 13:45:05 -0400

    logs working

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -10,11 +10,11 @@ cd ..
 # apply
 kubectl apply -f apps/
 # argocd login
-#ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o json | jq -r '.data.password' | base64 --decode --ignore-garbage)
-#argocd login localhost:8080 --username admin --password "$ARGOCD_PASSWORD" --insecure
+ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o json | jq -r '.data.password' | base64 --decode --ignore-garbage)
+argocd login localhost:8080 --username admin --password "$ARGOCD_PASSWORD" --insecure
 # argocd trigger sync
-#argocd app sync microservice-poc1
-#argocd app sync microservice-poc2
+argocd app sync microservice-poc1
+argocd app sync microservice-poc2
 # restart deployment
-kubectl rollout restart deployment -n microservice-poc1 microservice-poc1-app 
-kubectl rollout restart deployment -n microservice-poc2 microservice-poc2-app 
+kubectl rollout restart deployment -n microservice-poc1 microservice-poc1
+kubectl rollout restart deployment -n microservice-poc2 microservice-poc2
