255e867556328cedebbc21d78091b78c357a9293
Brandon Ros <brandonros1@gmail.com>
2023-05-10 13:33:23 -0400

    trying to get nlog to include traceid

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-# build
+# build + push
 cd MicroservicePOC
-docker build --build-arg ARTIFACTORY_USERNAME --build-arg ARTIFACTORY_PASSWORD -t azurepocsandboxaksacr.azurecr.io/microservice-poc:latest .
+docker build --progress=plain --build-arg ARTIFACTORY_USERNAME --build-arg ARTIFACTORY_PASSWORD -t azurepocsandboxaksacr.azurecr.io/microservice-poc:latest .
 docker push azurepocsandboxaksacr.azurecr.io/microservice-poc:latest
 cd ..
 # apply
 kubectl apply -f apps/
-# TODO: trigger argocd app sync?
-ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o json | jq -r '.data.password' | base64 --decode --ignore-garbage)
-argocd login localhost:8080 --username admin --password "$ARGOCD_PASSWORD" --insecure
-argocd app sync microservice-poc1
-argocd app sync microservice-poc2
+# argocd login
+#ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o json | jq -r '.data.password' | base64 --decode --ignore-garbage)
+#argocd login localhost:8080 --username admin --password "$ARGOCD_PASSWORD" --insecure
+# argocd trigger sync
+#argocd app sync microservice-poc1
+#argocd app sync microservice-poc2
+# restart deployment
+kubectl rollout restart deployment -n microservice-poc1 microservice-poc1-app 
+kubectl rollout restart deployment -n microservice-poc2 microservice-poc2-app 
